d0cc4fbf532a81fcd115f1d79f9ef5c5e7538154
billy07001 <72130484+billy07001@users.noreply.github.com>
2021-05-16 13:31:38 +0800

    final

--- src/component/sign.js
+++ src/component/sign.js
@@ -1,62 +1,142 @@
 import '../style/App.css';
 import '../style/sign.css';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Toast, Form, Button, Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 
-const allmsgs = [];
+function Signboard() {
+    return (
+        <canvas id="clothes"></canvas>
+    );
+}
+
+function signing() {
+    let canvas = document.querySelector('#clothes');
+    let ctx = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.strokeStyle = "#BADA55";
+    ctx.lineJoin = "round";
+    ctx.lineCap = "round";
+    ctx.lineWidth = 16;
+    let isDrawing = false;
+    let hue;
+    let lastX = 0;
+    let lastY = 0;
+    function move() {
+        hue = document.getElementById("slider").value;
+        document.getElementById("color").innerHTML = `${hue}`
+    }
+    let direction = true;
+    function draw(e) {
+        if (!isDrawing) return;
+        ctx.strokeStyle = `hsl(${hue}, 50%, 50%)`;
+        ctx.beginPath();
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+        // hue=0;
+        if (ctx.lineWidth >= 200 || ctx.lineWidth <= 1) {
+            direction = !direction;
+        }
+    }
+    canvas.addEventListener("mousedown", (e) => {
+        isDrawing = true;
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+    });
+
+    canvas.addEventListener("mousemove", draw);
+    canvas.addEventListener("mouseup", () => (isDrawing = false));
+    canvas.addEventListener("mouseout", () => (isDrawing = false));
+}
 
 function Sign() {
+    const [allmsgs, setAllmsgs] = useState([]);
+    const [nowmsgs, setNowmsgs] = useState("");
 
-    useEffect(() => {
+    function handleSubmit(e) {
+        const storage = window.localStorage;
+        const submit = JSON.stringify({
+            giver: storage.getItem("username"),
+            content: nowmsgs
+        });
+
+        console.log(submit);
+
         let headers = {
-            "Accept": "application/json"
+            "Content-Type": "application/json",
+            "Accept": "application/json",
         }
+        e.preventDefault()
         fetch('http://localhost:3000/api/blogs', {
-            method: "GET",
+            method: "POST",
             headers,
+            body: submit,
         })
             .then(res => res.json())
             .then(json => {
-                json.msgs.map((msg) => {
-                    allmsgs.push({ giver: msg.giver, content: msg.content });
-                })
+                console.log(json)
             });
-    });
-    const all = allmsgs.map((allmsg, index) => {
-        return (
-            <Col className="message">
-                <Toast key={index}>
-                    <Toast.Header>
-                        <strong className="mr-auto">{allmsg.giver}</strong>
-                        <small>11 mins ago</small>
-                    </Toast.Header>
-                    <Toast.Body>{allmsg.content}</Toast.Body>
-                </Toast>
-            </Col>
-        )
+    }
+
+
+
+    useEffect(() => {
+        let ignore = false;
+
+        async function fetchData() {
+            if (!ignore) {
+                const result = await axios('http://localhost:3000/api/blogs');
+                setAllmsgs(result.data.msgs);
+            }
+        }
+
+        fetchData();
+        return () => { ignore = true };
     });
 
-    console.log(allmsgs);
+    useEffect(() => { signing() })
+
+
     return (
         <div className="App-header">
-            <div className="messageform">
-                <Form>
-                    <Form.Group controlId="exampleForm.ControlTextarea1">
-                        <Form.Label>輸入留言</Form.Label>
-                        <Form.Control as="textarea" rows={3} />
-                    </Form.Group>
+            <div>
+                <Container className="messageform">
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>輸入留言</Form.Label>
+                            <Form.Control as="textarea" rows={3} value={nowmsgs} onChange={e => setNowmsgs(e.target.value)} />
+                        </Form.Group>
 
-                    <Button variant="primary" type="submit">
-                        送出
+                        <Button variant="primary" type="submit">
+                            送出
                     </Button>
-                </Form>
+                    </Form>
+                </Container>
             </div>
             <div>
                 <Container>
                     <Row>
-                        {all}
+                        {allmsgs.map((allmsg) => (
+                            <Col className="message">
+                                <Toast>
+                                    <Toast.Header>
+                                        <strong className="mr-auto">{allmsg.giver}</strong>
+                                    </Toast.Header>
+                                    <Toast.Body>{allmsg.content}</Toast.Body>
+                                </Toast>
+                            </Col>
+                        )
+                        )}
                     </Row>
                 </Container>
+            </div>
+
+            <div className="signboard">
+                <p>留下感人的一句話</p>
+                <Signboard />
             </div>
         </div>
     );
--- src/component/take.js
+++ src/component/take.js
@@ -1,15 +1,34 @@
 import '../style/App.css';
 import '../style/takepage.css';
-import diploma from '../view/diploma.jpg';
+import '../style/main.css';
+import logo from '../view/logo.png';
+import ocean from '../view/ocean.png';
+import president from '../view/president.png';
 
 const studentName = window.localStorage.getItem("username");
+const nowdate = new Date();
+const now = (nowdate.getMonth() + 1) + "/" + nowdate.getDate() + "/" + nowdate.getFullYear();
 
 function Take() {
     return (
         <div className="App-header">
             <div>
-                <img className="diploma" src={diploma} alt="Diploma" />
-                <p>姓名：{studentName}</p>
+                <div class="container box-shadow">
+                    <div class="topimg">
+                        <img class="university" src={logo} alt="represent" />
+                    </div>
+                    <header class="header">
+                        <p class="certificate">Certificate</p>
+                    </header>
+
+                    <div class="contentdiv">
+                        <p class="content">Congruation, Mr./Mrs	{studentName} complete all of four years courses in National Taiwan Ocean University from department of Management information . It is given in Taiwan ,day of {now} .</p>
+                    </div>
+                    <div class="imginline">
+                        <img class="president" src={president} alt="president" />
+                        <img class="ocean" src={ocean} alt="ocaen" />
+                    </div>
+                </div>
             </div>
         </div>
     );
